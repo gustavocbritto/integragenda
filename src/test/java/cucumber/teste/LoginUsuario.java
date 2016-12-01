7af5863aab812e5537164babbff8f7664aa04bfc
Gustavo Britto <gustavocbritto@gmail.com>
2016-12-01 02:01:50 -0200

    teste cucumber

--- src/test/java/cucumber/teste/LoginUsuario.java
+++ src/test/java/cucumber/teste/LoginUsuario.java
@@ -1,33 +1,32 @@
 package cucumber.teste;
 
-import integragenda.modelo.Belly;
-import cucumber.api.PendingException;
+import model.Pessoa;
+import model.Usuario;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 
 public class LoginUsuario {
-	
+	private Pessoa pessoa = new Pessoa();
+	private Usuario usuario = new Usuario();
 	@Given("^Eu entro com meu usuario \"([^\"]*)\" e senha \"([^\"]*)\"$")
 	public void euEntroComMeuUsuarioESenha(String arg1, String arg2) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		usuario.setPessoa(pessoa);
+		pessoa.setNome(arg1);
+		pessoa.setSenha(arg2);
 	}
 	
 	@When("^Eu clico em entrar$")
 	public void euClicoEmEntrar() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+			usuario.verifica();
 	}
 
 	@Then("^Meu usuario e logado no sistema$")
 	public void meuUsuarioELogadoNoSistema() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+			assertThat(usuario.getNomeAtual(), is("Gustavo"));
 	}
 	
 }
