f1490439906bdef4b0dc27d7950abd928c13639d
Gustavo Britto <gustavocbritto@gmail.com>
2016-12-02 00:48:07 -0200

    Novo teste UsuarioAcessAsSalas

--- src/test/java/cucumber/teste/CadastroUsuario.java
+++ src/test/java/cucumber/teste/CadastroUsuario.java
@@ -2,7 +2,6 @@ package cucumber.teste;
 
 import model.Pessoa;
 import model.Usuario;
-import persistence.PessoaDAO;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
--- src/test/java/cucumber/teste/UsuarioAcessoAsSuasSalas.java
+++ src/test/java/cucumber/teste/UsuarioAcessoAsSuasSalas.java
@@ -1,6 +1,10 @@
 package cucumber.teste;
 
 import integragenda.modelo.Belly;
+import model.ControleMinhasSalasBean;
+import model.Pessoa;
+import model.Usuario;
+import model.Sala;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -9,25 +13,36 @@ import cucumber.api.java.en.When;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class UsuarioAcessoAsSuasSalas {
-	
+	private Usuario usuario = new Usuario();
+	private ControleMinhasSalasBean controle = new ControleMinhasSalasBean();
+	private Pessoa pessoa = new Pessoa();
 	@Given("^Eu estou logado no sistema com algumas salas cadastradas$")
 	public void euEstouLogadoNoSistemaComAlgumasSalasCadastradas() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		pessoa.setNome("Gustavo");
+		pessoa.setSenha("123456");
+		usuario.setPessoa(pessoa);
+		usuario.entrar();
+		controle.setUsuario(usuario);
 	}
 
 	@When("^Eu clico em minhas Salas$")
 	public void euClicoEmMinhasSalas() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		controle.selecionaSalas();
 	}
 
 	@Then("^Eu recebo uma lista com minhas salas$")
 	public void euReceboUmaListaComMinhasSalas() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    List<Sala> listaSalas = new ArrayList<Sala>();
+	    listaSalas = controle.getSalas();
+	    for(Sala sala : listaSalas)
+	    {
+	    	assertThat(sala.getPessoa().getNome(), is(usuario.getPessoa().getNome()));
+	    }
 	}
 	
 }
