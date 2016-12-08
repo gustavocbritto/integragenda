4637e0f2e9bc3bf7aaa65e0085bc03b3dd8b39d2
Gustavo Britto <gustavocbritto@gmail.com>
2016-12-08 10:11:59 -0200

    Tentativa teste cucumber AdminEditTodasAsSalas

--- src/test/java/cucumber/teste/AdminEditTodasAsSalas.java
+++ src/test/java/cucumber/teste/AdminEditTodasAsSalas.java
@@ -1,6 +1,11 @@
 package cucumber.teste;
 
 import integragenda.modelo.Belly;
+import model.ControleMinhasSalasBean;
+import model.ControleSalasBean;
+import model.Pessoa;
+import model.Sala;
+import model.Usuario;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -9,25 +14,46 @@ import cucumber.api.java.en.When;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class AdminEditTodasAsSalas {
-
+	
+	private Usuario usuario = new Usuario();
+	private ControleMinhasSalasBean controleMinhasSalas = new ControleMinhasSalasBean();
+	private ControleSalasBean controleSalas = new ControleSalasBean();
+	private Pessoa pessoa = new Pessoa();
+	
 	@Given("^Eu entrei no sistema como adminstrador$")
 	public void euEntreiNoSistemaComoAdminstrador() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		
+		pessoa.setNome("Administrador");
+		pessoa.setSenha("123456");
+		usuario.setPessoa(pessoa);
+		usuario.entrar();
+		controleMinhasSalas.setUsuario(usuario);
+		controleSalas.setUsuario(usuario);
+		
 	}
 
 	@When("^Eu clico em minhas salas$")
 	public void euClicoEmMinhasSalas() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		
+		controleMinhasSalas.selecionaSalas();
+		controleSalas.selecionaSalas();
 	}
 
 	@Then("^Eu posso editar todas as salas listadas$")
 	public void euPossoEditarTodasAsSalasListadas() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		
+	    List<Sala> listaMihasSalas = new ArrayList<Sala>();
+	    List<Sala> listaSalas =  new ArrayList<Sala>();
+	    listaSalas = controleSalas.getSalas();
+	    listaMihasSalas = controleMinhasSalas.getSalas();
+	    
+	    assertThat(listaSalas, is(listaMihasSalas));
+	    
 	}
 	
 }
