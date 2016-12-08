8ee11723850a58d87c7e5eb1a9c72ae1b664d4a0
Gustavo Britto <gustavocbritto@gmail.com>
2016-12-08 11:27:41 -0200

    teste cucumber usuarioTemAcessoAsInformacoesDaSala

--- src/test/java/cucumber/teste/UsuarioTemAcessoAsInformacoesDaSala.java
+++ src/test/java/cucumber/teste/UsuarioTemAcessoAsInformacoesDaSala.java
@@ -1,33 +1,67 @@
 package cucumber.teste;
 
-import integragenda.modelo.Belly;
-import cucumber.api.PendingException;
+import model.ControleSalasBean;
+import model.Pessoa;
+import model.Sala;
+import model.Usuario;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class UsuarioTemAcessoAsInformacoesDaSala {
 
+	private Usuario usuario = new Usuario();
+	private ControleSalasBean controleSalas = new ControleSalasBean();
+	private Pessoa pessoa = new Pessoa();
+	
 	@Given("^Eu tenho a lista de salas$")
 	public void euTenhoAListaDeSalas() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		
+		pessoa.setNome("Gustavo");
+		pessoa.setSenha("123456");
+		usuario.setPessoa(pessoa);
+		usuario.entrar();
+		controleSalas.setUsuario(usuario);
+		
 	}
 
 	@When("^Eu clico em alguma sala$")
 	public void euClicoEmAlgumaSala() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		
+		//bean responsavel por pegar todas as salas
+		controleSalas.selecionaSalas();
+		
 	}
 
 	@Then("^Eu recebo as informacoes sobre a sala selecionada$")
 	public void euReceboAsInformacoesSobreASalaSelecionada() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		
+	    List<Sala> listaSalas =  new ArrayList<Sala>();
+	    listaSalas = controleSalas.getSalas();
+	    
+	    //Aqui eu verifico se atravez das salas eu tenho as informacoes referentes a elas
+	    for(Sala sala : listaSalas)
+	    {
+	    	assertThat(sala.getCategoria().getDescricao(), not(equalTo("")));
+	    	assertThat(sala.getDescricao(), not(equalTo("")));
+	    	assertThat(sala.getPreco() >= 0, is(true));
+	    	assertThat(sala.getEstrela() >= 0, is(true));
+	    	assertThat(sala.getImagens().size() > 0, is(true));
+	    	assertThat(sala.getTamanhoMax() > 0, is(true));
+	    	assertThat(sala.getUtensilios().size() >= 0, is(true));
+	    	assertThat(sala.getLocalizacao().getCidade(), not(equalTo("")));
+	    	assertThat(sala.getLocalizacao().getRua(), not(equalTo("")));
+	    	assertThat(sala.getLocalizacao().getEstado(), not(equalTo("")));
+	    	assertThat(sala.getLocalizacao().getPais(), not(equalTo("")));
+	    	assertThat(sala.getLocalizacao().getNumero() > 0, is(true));
+	    }
+	    
+	    
 	}
 	
 }
