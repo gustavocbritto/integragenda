5e696264b1825d4de03d5e09327d82e63938ce58
Gustavo Britto <gustavocbritto@gmail.com>
2016-12-08 11:02:54 -0200

    teste cucumber adminVisualizarTodasAsSalas

--- src/test/java/cucumber/teste/AdminVisualizarTodasAsSalas.java
+++ src/test/java/cucumber/teste/AdminVisualizarTodasAsSalas.java
@@ -1,33 +1,49 @@
 package cucumber.teste;
 
-import integragenda.modelo.Belly;
-import cucumber.api.PendingException;
+import model.ControleSalasBean;
+import model.Pessoa;
+import model.Sala;
+import model.Usuario;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class AdminVisualizarTodasAsSalas {
 
+	private Usuario usuario = new Usuario();
+	private ControleSalasBean controleSalas = new ControleSalasBean();
+	private Pessoa pessoa = new Pessoa();
+	
 	@Given("^Eu entro no sistema como Administrador$")
 	public void euEntroNoSistemaComoAdministrador() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		pessoa.setNome("Administrador");
+		pessoa.setSenha("123456");
+		usuario.setPessoa(pessoa);
+		usuario.entrar();
+		controleSalas.setUsuario(usuario);
 	}
 
 	@When("^Eu clico em todas as salas$")
 	public void euClicoEmTodasAsSalas() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+
+		//bean responsavel por pegar todas as salas
+		controleSalas.selecionaSalas();
+		
 	}
 
 	@Then("^Eu recebo uma lista com todas as salas$")
 	public void euReceboUmaListaComTodasAsSalas() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		
+	    List<Sala> listaSalas =  new ArrayList<Sala>();
+	    listaSalas = controleSalas.getSalas();
+	    
+	    //Verifico se a quantidade de salas eh a mesma quantidade de salas que posso editar
+	    assertThat(listaSalas.size(), is(8));
 	}
 	
 }
