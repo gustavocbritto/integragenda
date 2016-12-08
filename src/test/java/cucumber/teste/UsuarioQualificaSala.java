e8ce17c9a965b41666cf7982ccd0bdd97446d958
Gustavo Britto <gustavocbritto@gmail.com>
2016-12-08 18:25:52 -0200

    fatou subir o teste

--- src/test/java/cucumber/teste/UsuarioQualificaSala.java
+++ src/test/java/cucumber/teste/UsuarioQualificaSala.java
@@ -1,51 +1,62 @@
 package cucumber.teste;
 
-import integragenda.modelo.Belly;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-
+import model.ControleSalasBean;
+import model.Pessoa;
+import model.Sala;
+import model.Usuario;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class UsuarioQualificaSala {
 	
+	private Usuario usuario = new Usuario();
+	private ControleSalasBean controleSalas = new ControleSalasBean();
+	private Pessoa pessoa = new Pessoa();
+	
 	@Given("^Eu desejo avaliar uma sala que eu ja tenha alugado$")
 	public void euDesejoAvaliarUmaSalaQueEuJaTenhaAlugado() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		pessoa.setNome("Administrador");
+		pessoa.setSenha("123456");
+		usuario.setPessoa(pessoa);
+		usuario.entrar();
+		controleSalas.setUsuario(usuario);
 	}
 	
 	@When("^Eu seleciono a pontuacao (\\d+) para a sala$")
 	public void euSelecionoAPontuacaoParaASala(int arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		//bean responsavel por pegar todas as salas
+		controleSalas.selecionaSalas();
 	}
 
 	@Then("^A pontuacao da sala e atualizada com meu voto$")
 	public void aPontuacaoDaSalaEAtualizadaComMeuVoto() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
-
-	@Given("^Eu desejo avaliar uma sala que eu nao tenha alugado$")
-	public void euDesejoAvaliarUmaSalaQueEuNaoTenhaAlugado() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
-	
-	@When("^Seleciono a pontuacao (\\d+) para a sala$")
-	public void selecionoAPontuacaoParaASala(int arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
-	
-	@Then("^A pontuacao da sala nao e atualizada com meu voto$")
-	public void aPontuacaoDaSalaNaoEAtualizadaComMeuVoto() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		
+	    List<Sala> listaSalas =  new ArrayList<Sala>();
+	    listaSalas = controleSalas.getSalas();
+	    for(Sala sala: listaSalas)
+	    {
+	    	sala.setEstrela(5);
+	    }
+	    
+		//bean responsavel por pegar todas as salas
+		controleSalas.selecionaSalas();
+		List<Sala> listaSalasAtualizada =  new ArrayList<Sala>();
+		listaSalasAtualizada = controleSalas.getSalas();
+		
+		//Verifico se a quantidade de estrela que esta salvo eh a mesma que tinha coloado
+	    for(Sala sala: listaSalasAtualizada)
+	    {
+	    	assertThat(sala.getEstrela(), is(5));
+	    }
+	    
+	    
+	    
 	}
 	
 }
